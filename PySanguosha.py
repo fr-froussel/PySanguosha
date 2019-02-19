17d6ff6af29f56949dad1cc12e0c4e4e1c469097
Florian Roussel <froussel@eos-imaging.com>
2019-02-19 12:08:35 +0100

    Adding one test spell.

--- PySanguosha.py
+++ PySanguosha.py
@@ -1,4 +1,4 @@
-import TextWrapper
+from TextWrapper import TextWrapper
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -25,15 +25,60 @@ for magatama_number in range(8):
   magatama_previous_width_position = magatama_width_base_position + (int(magatama_w * (magatama_number * 0.5))) + (magatama_number * magatama_spacement)
   img_final.paste(magatama, (magatama_previous_width_position, magamatame_heigth_position), mask=magatama)
 
-# Nom
-img_name = Image.new("RGB", (100, 100))
-draw = ImageDraw.Draw(img_name)
-font = ImageFont.truetype("arial.ttf", 16)
-draw.text((0, 0), "Sample Text", (255,255,255), font=font, direction='ttb')
-#
-img_name.save('out_name.png')
+# img_name = Image.new('RGB', (100, 100))
+# draw = ImageDraw.Draw(img_name)
+# font = ImageFont.truetype('arial.ttf', 16)
+# draw.text((0, 0), "Sample Text", (255,255,255), font=font)
+# img_name.save('out_name.png')
 
+spells_base_pos = [85, 375]
+spells_max_size = (250, 480)
+
+spell_name_test = '司敵'
+spell_text_test = 'Whenever Cao Zhen uses an escape or any other player uses an escape during Cao Zhen\'s turn, he puts the top card of the deck on his general card. At the beginning of any other players action phase, Cao Zhen can discard one of these cards to reduce the number of attacks that the current player can use by one.'
+
+# create the ImageFont instance
+font = ImageFont.truetype('resources/font/arial.ttf', size=TextWrapper.pixel_to_points(15), encoding='unic')
+lines = TextWrapper.wrap_text_by_width(spell_text_test, font, spells_max_size[0])
+_, lines_size = TextWrapper.text_size(lines, font)
+
+# go write text if condition is ok
+if lines_size[1] < spells_max_size[1]:
+    current_text_pos = spells_base_pos
+    draw = ImageDraw.Draw(img_final)
+
+    # skill name UI
+    skill = Image.open('./resources/cards/front/god-skill-new.png')
+    skill_size = skill.size
+    skill_pos = (current_text_pos[0] - skill_size[0], current_text_pos[1])
+    img_final.paste(skill, skill_pos, mask=skill)
+
+    # skill name
+    spell_font = ImageFont.truetype('resources/font/SimSun.ttf', size=TextWrapper.pixel_to_points(20), encoding='unic')
+    _, spell_name_size = TextWrapper.text_size(spell_name_test, spell_font)
+    spell_name_pos = (skill_pos[0] + 7,
+                      skill_pos[1] + int(skill_size[1] / 2) + 1 - int(spell_name_size[1] / 2))
+
+    draw.text(spell_name_pos, spell_name_test, (255, 255, 255), font=spell_font)
+
+    # skill text middle UI
+    skill_middle = Image.open('./resources/cards/front/god-skill-middle.png')
+    skill_middle = skill_middle.resize((skill_middle.size[0], lines_size[1]))
+    img_final.paste(skill_middle, current_text_pos, mask=skill_middle)
+
+    # skill text up UI
+    skill_up = Image.open('./resources/cards/front/god-skill-up.png')
+    img_final.paste(skill_up, (current_text_pos[0], current_text_pos[1] - skill_up.size[1]), mask=skill_up)
+
+    # skill text down UI
+    skill_down = Image.open('./resources/cards/front/god-skill-down.png')
+    img_final.paste(skill_down, (current_text_pos[0], current_text_pos[1] + skill_middle.size[1]), mask=skill_down)
+
+    # loop through lines to draw
+    for line in lines:
+        draw.text((current_text_pos[0] + 7, current_text_pos[1]), line, (0, 0, 0), font=font)
+        _, text_size = TextWrapper.text_size(line, font)
+        current_text_pos[1] += text_size[1]
+
+# save final composition
 img_final.save('out.png')
-
-lines = TextWrapper.draw_text("coucou ceci est un test", 500)
-print("Lines wrapped:", lines)
--- TextWrapper.py
+++ TextWrapper.py
@@ -1,35 +1,77 @@
-from PIL import ImageFont
+class TextWrapper:
+    @staticmethod
+    def wrap_text_by_width(text, font, max_width):
+        """
+        Wrap text based on a maximum width
+        :param text: text to wrap
+        :param font: font used to calculation
+        :param max_width: maximum width
+        :return: text wrapped
+        """
+        lines = []
 
-def text_wrap(text, font, max_width):
-  lines = []
-  # If the width of the text is smaller than image width
-  # we don't need to split it, just add it to the lines array
-  # and return
-  if font.getsize(text)[0] <= max_width:
-    lines.append(text)
-  else:
-    # split the line by spaces to get words
-    words = text.split(' ')
-    i = 0
-    # append every word to a line while its width is shorter than image width
-    while i < len(words):
-      line = ''
-      while i < len(words) and font.getsize(line + words[i])[0] <= max_width:
-        line = line + words[i] + " "
-        i += 1
-      if not line:
-        line = words[i]
-        i += 1
-      # when the line gets longer than the max width do not append the word,
-      # add the line to the lines array
-      lines.append(line)
-  return lines
+        # If the width of the text is smaller than image width
+        # we don't need to split it, just add it to the lines array
+        # and return
+        if font.getsize(text)[0] <= max_width:
+            lines.append(text)
+        else:
+            # split the line by spaces to get words
+            words = text.split(' ')
+            i = 0
+            # append every word to a line while its width is shorter than image width
+            while i < len(words):
+                line = ''
+                while i < len(words) and font.getsize(line + words[i])[0] <= max_width:
+                    line = line + words[i] + " "
+                    i += 1
+                if not line:
+                    line = words[i]
+                    i += 1
+                # when the line gets longer than the max width do not append the word,
+                # add the line to the lines array
+                lines.append(line)
 
+        return lines
 
-def draw_text(text, max_width):
-  # create the ImageFont instance
-  font_file_path = 'resources/font/arial.ttf'
-  font = ImageFont.truetype(font_file_path, size=50, encoding="unic")
+    @staticmethod
+    def text_size(lines, font):
+        """
+        Text wrapped height calculation
+        :param lines: lines wrapped from TextWrapper.wrap_text_by_width
+        :param font: font size to width calculation
+        :return: height in pixel
+        """
+        height_calculation_status = False
+        height = 0
+        width = 0
 
-  # get shorter lines
-  return text_wrap(text, font, max_width)
+        if lines:
+            if isinstance(lines, list):
+                for line in lines:
+                    width += font.getsize(line)[0]
+                    height += font.getsize(line)[1]
+            else:
+                width += font.getsize(lines)[0]
+                height += font.getsize(lines)[1]
+            height_calculation_status = True
+
+        return height_calculation_status, (width, height)
+
+    @staticmethod
+    def points_to_pixel(points):
+        """
+        Convert points value to pixel value
+        :param points: points value
+        :return: pixel value
+        """
+        return int(points / 0.75)
+
+    @staticmethod
+    def pixel_to_points(pixel):
+        """
+        Convert pixel value to points value
+        :param pixel: pixel value
+        :return: points value
+        """
+        return int(pixel * 0.75)
