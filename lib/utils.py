7f88f9b05b82fd4b2effcec3c9f5d9911b39af7c
Florian Roussel <froussel@eos-imaging.com>
2019-02-27 13:24:12 +0100

    Usefull constants from base resources.

--- lib/utils.py
+++ lib/utils.py
@@ -113,3 +113,9 @@ clan_ui_resources_name = \
 spells_base_pos = [87, 375]
 
 max_spells_size = (225, 215)
+
+background_base_pos = (65, 50)
+background_size = (275, 345)
+main_border = (62, 12, 32, 15)
+spell_name_base_pos = (10, 4)
+spell_name_size = (41, 16)
